6e64c540fb542f22dd974f71052515215d3c383b
Ayan Banerjee <108017943+AyanBanerjee280@users.noreply.github.com>
2022-07-17 23:56:36 +0530

    Add files via upload

---
+++ Delegates_4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace DelegatesinCSharp
+{
+    class Del3
+    {
+        public delegate void addnum(int num1);
+        
+        public void table(int num1)
+        {
+            for (int i = 1; i <= 200; i++)
+            {
+                Console.WriteLine("{0} * {1} = {2}",num1, i, num1*i);
+            }
+        }
+        
+
+   
+        public static void Main(string[] args)
+        {
+            Del3 obj = new Del3();
+            addnum del_obj1 = new addnum(obj.table);
+            del_obj1(9);
+            Console.ReadLine();
+        }
+    }
+}
